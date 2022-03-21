8307fa1988ff0c722036b3de044ea0accc8743e3
Diego Martín Campos <71278795+diegomartincp@users.noreply.github.com>
2022-03-21 13:45:43 +0100

    Tweepy fecha y texto

---
+++ tweepy_data.py
@@ -0,0 +1,13 @@
+import tweepy
+import pandas as pd
+
+bearer_token = 'AAAAAAAAAAAAAAAAAAAAAEEuaQEAAAAAmJVCeL1N%2FiSo9G6YVLB%2FhPoHjjg%3DkfiN8gv5mGV6WEHsle74BxyyQkAdJt1O6ET4WuarlWk5JTKN57'
+client = tweepy.Client(bearer_token=bearer_token)
+
+
+response = client.search_recent_tweets(query="Tres Cantos",tweet_fields = ['created_at'], max_results=10)
+
+for tweet in response.data:
+    print(tweet.text)
+    print(tweet.created_at.date()) #.created_at devuelve datetime
+
